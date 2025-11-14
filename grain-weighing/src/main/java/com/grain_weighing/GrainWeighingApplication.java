c5929d29eeaa8baac3839db812c37ce91e0dc12c
Israel.Sestrem <israel.sestrem@senior.com.br>
2025-11-14 15:17:43 -0300

    [INIT] initial project with Spring

---
+++ grain-weighing/src/main/java/com/grain_weighing/GrainWeighingApplication.java
@@ -0,0 +1,13 @@
+package com.grain_weighing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainWeighingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrainWeighingApplication.class, args);
+	}
+
+}
---
+++ grain-weighing/src/test/java/com/grain_weighing/GrainWeighingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grain_weighing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainWeighingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
