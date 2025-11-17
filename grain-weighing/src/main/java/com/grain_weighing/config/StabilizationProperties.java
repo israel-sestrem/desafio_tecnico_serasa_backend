748290cee8b7763104cdcb05085c80e770cdd98a
Israel.Sestrem <israel.sestrem@senior.com.br>
2025-11-17 12:50:35 -0300

    [CHANGE] making StabilizationProperties a component

--- grain-weighing/src/main/java/com/grain_weighing/config/StabilizationProperties.java
+++ grain-weighing/src/main/java/com/grain_weighing/config/StabilizationProperties.java
@@ -2,11 +2,13 @@ package com.grain_weighing.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@ConfigurationProperties(prefix = "grain-weighing.stabilization")
 @Data
+@Component
+@ConfigurationProperties(prefix = "grain-weighing.stabilization")
 public class StabilizationProperties {
     private int requiredStableReadings;
     private BigDecimal maxDiffBetweenReadingsKg;
